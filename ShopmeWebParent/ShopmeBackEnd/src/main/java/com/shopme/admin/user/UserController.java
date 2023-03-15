08e1df4c54796b90705ac312c317cd4fbbba33c6
4kin-smp <4kin.nikita+smp@gmaill.com>
2023-03-15 17:12:18 +0300

    007 Code New User Function.mp4 18 min

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -4,6 +4,7 @@ import com.shopme.common.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -16,10 +17,23 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public String listAll(Model model){
+    public String listAll(Model model) {
         List<User> listUsers = service.listAll();
-        model.addAttribute("listUsers" , listUsers);
+        model.addAttribute("listUsers", listUsers);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        System.out.println(user);
+        return "redirect:/users";
     }
 }
